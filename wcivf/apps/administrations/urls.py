5015a3cf572d09c5ffa8ed5dac9cfe8fac811ee5
Sym Roe <sym.roe@talusdesign.co.uk>
2024-07-05 05:46:25 +0100

    New view for admins

---
+++ wcivf/apps/administrations/urls.py
@@ -0,0 +1,16 @@
+from django.urls import re_path
+
+from .views import YourArea
+
+urlpatterns = [
+    re_path(
+        r"^(?P<postcode>[^/]+)/(?P<uprn>[^/]+)/$",
+        YourArea.as_view(),
+        name="your_area_view",
+    ),
+    re_path(
+        r"^(?P<postcode>[^/]+)/$",
+        YourArea.as_view(),
+        name="your_area_view",
+    ),
+]
