6e1bdd045ad72ef6aec6e6432d3f87174e9198f3
chris48s <git@chris-shaw.dev>
2024-08-13 15:55:23 +0100

    move existing local.example to expected location

--- wcivf/settings/local.example.py
+++ wcivf/settings/local.example.py
@@ -1,14 +1,38 @@
-DEBUG = True
-
-SECRET_KEY = "asdasdasdasdasdasdasd"
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": "",
-        "USER": "",
-        "PASSWORD": "",
-        "HOST": "",
-        "PORT": "",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "wcivf",
+        "USER": "wcivf",
+        "PASSWORD": "wcivf",
     }
 }
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+ALLOWED_HOSTS = ["*"]
+
+YNR_BASE = "https://candidates.democracyclub.org.uk"
+
+EE_BASE = "https://elections.democracyclub.org.uk"
+
+INTERNAL_IPS = ['127.0.0.1']
+
+SECRET_KEY = "not_for_production"
+
+DEBUG = True
+
+EMAIL_SIGNUP_BACKEND = "local_db"
+
+# SHOW_GB_ID_MESSAGING = False
+# SHOW_RESULTS_CHART = False
+
+# These example paths should only be necessary for M1 Mac users
+
+# GDAL_LIBRARY_PATH = "/opt/homebrew/Cellar/gdal/3.5.1_3/lib/libgdal.dylib"
+
+# GEOS_LIBRARY_PATH= '/opt/homebrew/Cellar/geos/3.11.0/lib/libgeos_c.1.17.0.dylib'
