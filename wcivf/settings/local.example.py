416fd840953331d85fbdab6efaef62d37889f5ae
chris48s <git@chris-shaw.dev>
2024-08-13 15:22:43 +0100

    add a template local.example.py

---
+++ wcivf/settings/local.example.py
@@ -0,0 +1,14 @@
+DEBUG = True
+
+SECRET_KEY = "asdasdasdasdasdasdasd"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    }
+}
