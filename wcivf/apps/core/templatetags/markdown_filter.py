b5f65a2dc1906efd311b257f734aedbf532a8278
awdem <wdemontmollin@gmail.com>
2025-03-20 16:24:42 +0000

    update markdown filter

--- wcivf/apps/core/templatetags/markdown_filter.py
+++ wcivf/apps/core/templatetags/markdown_filter.py
@@ -1,4 +1,5 @@
 import markdown
+import nh3
 from django import template
 from django.utils.safestring import mark_safe
 
@@ -7,7 +8,8 @@ register = template.Library()
 
 @register.filter(name="markdown")
 def markdown_filter(text):
-    return mark_safe(markdown.markdown(text))
+    clean_text = nh3.clean(text)
+    return mark_safe(markdown.markdown(clean_text, extensions=["nl2br"]))
 
 
 markdown_filter.is_safe = True
