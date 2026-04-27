4304e3b40e64aae4997f2118adebab90f3118b7b
chris48s <git@chris-shaw.dev>
2026-04-27 15:36:55 +0100

    expect statement_to_voters_last_updated field in YNR API

--- wcivf/apps/people/managers.py
+++ wcivf/apps/people/managers.py
@@ -127,17 +127,13 @@ class PersonManager(models.Manager):
         }
 
         if defaults["statement_to_voters"] is not None:
-            try:
-                defaults.update(
-                    {
-                        "statement_to_voters_last_updated": person[
-                            "statement_to_voters_last_updated"
-                        ]
-                    }
-                )
-            except KeyError:
-                print("No statement_to_voters_last_updated field found")
-                pass
+            defaults.update(
+                {
+                    "statement_to_voters_last_updated": person[
+                        "statement_to_voters_last_updated"
+                    ]
+                }
+            )
 
         for value_type in VALUE_TYPES_TO_IMPORT:
             defaults[value_type] = None
