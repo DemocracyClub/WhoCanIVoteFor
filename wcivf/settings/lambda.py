d0686e3c5f2f55af37fb3e3c739ffb5821465fbf
awdem <wdemontmollin@gmail.com>
2026-04-28 11:30:42 +0100

    overwrite existing DATABASES on lambda

--- wcivf/settings/lambda.py
+++ wcivf/settings/lambda.py
@@ -2,12 +2,15 @@ import os
 
 from .base import *  # noqa
 
-DATABASES["default"] = {  # noqa
-    "ENGINE": "django.db.backends.postgresql",
-    "NAME": os.environ.get("RDS_DB_NAME"),
-    "USER": "wcivf",
-    "PASSWORD": os.environ.get("RDS_DB_PASSWORD"),
-    "HOST": os.environ.get("RDS_HOST"),
-    "PORT": os.environ.get("RDS_DB_PORT", "5432"),
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("RDS_DB_NAME"),
+        "USER": "wcivf",
+        "PASSWORD": os.environ.get("RDS_DB_PASSWORD"),
+        "HOST": os.environ.get("RDS_HOST"),
+        "PORT": os.environ.get("RDS_DB_PORT", "5432"),
+    }
 }
+
 EE_BASE = "https://elections.democracyclub.org.uk"
