96620bca7b8a41958fd707fbe4e34ae270caa6c5
chris48s <git@chris-shaw.dev>
2026-07-14 07:39:26 +0100

    use tz aware now

--- wcivf/apps/leaflets/models.py
+++ wcivf/apps/leaflets/models.py
@@ -1,13 +1,14 @@
 import datetime as dt
 
 from django.db import models
+from django.utils import timezone
 from people.models import Person
 
 
 class LeafletQuerySet(models.QuerySet):
     def latest_four(self):
         return self.filter(
-            date_uploaded_to_electionleaflets__gte=dt.datetime.now()
+            date_uploaded_to_electionleaflets__gte=timezone.now()
             - dt.timedelta(days=365)
         ).order_by("-date_uploaded_to_electionleaflets")[:4]
 
