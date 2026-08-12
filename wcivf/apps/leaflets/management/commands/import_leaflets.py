45cefd05914b0d6777ad93f9d78bc4ed479b328c
chris48s <git@chris-shaw.dev>
2026-08-12 11:10:43 +0100

    update import_leaflets to use current flag

--- wcivf/apps/leaflets/management/commands/import_leaflets.py
+++ wcivf/apps/leaflets/management/commands/import_leaflets.py
@@ -5,76 +5,28 @@ from dateutil.parser import parse
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from django.utils import timezone as tz
-from django.utils.http import urlencode
-from elections.models import PostElection
 from leaflets.models import Leaflet
 from people.models import Person
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "--recently-uploaded",
-            action="store_true",
-            dest="recently_uploaded",
-            help="Import leaflets uploaded since last imported Leaflet",
-        )
-        parser.add_argument(
-            "--uploaded-since",
-            action="store",
-            dest="uploaded_since",
-            help="Import leaflets uploaded since last imported Leaflet",
-        )
-
     @transaction.atomic
     def handle(self, **options):
         base_url = "https://electionleaflets.org/api/leaflets"
 
-        qs = PostElection.objects.filter(election__current=True)
-        if options["recently_uploaded"]:
-            # delete non current leaflets
-            Leaflet.objects.exclude(
-                person__personpost__post_election__in=qs
-            ).delete()
-
-            latest_upload = Leaflet.objects.order_by(
-                "-date_uploaded_to_electionleaflets"
-            ).first()
-
-            since = options.get("uploaded_since")
-            if since and " " not in since:
-                since = f"{since} 00:00"
-            if not since:
-                try:
-                    since = latest_upload.date_uploaded_to_electionleaflets
-                except AttributeError:
-                    since = None
-
-            params = {}
-            if since:
-                params = {"modified__gt": since}
-            url = f"{base_url}/?{urlencode(params)}"
-            while url:
-                req = requests.get(url)
-                req.raise_for_status()
-                results = req.json()
-                url = results.get("next", None)
-                self.add_leaflets(results.get("results", []))
-        else:
-            Leaflet.objects.all().delete()
-            for ballot in qs:
-                url = f"{base_url}/?ballot={ballot.ballot_paper_id}"
-                while url:
-                    req = requests.get(url)
-                    if req.status_code == 200:
-                        results = req.json()
-                        url = results.get("next", None)
-                        self.add_leaflets(results.get("results", []))
-                    else:
-                        url = None
+        Leaflet.objects.all().delete()
+        url = f"{base_url}/?current=true"
+        while url:
+            print(url)
+            resp = requests.get(url)
+            resp.raise_for_status()
+            results = resp.json()
+            url = results.get("next", None)
+            self.add_leaflets(results.get("results", []))
 
     def add_leaflets(self, results):
         for leaflet in results:
+            print(leaflet["pk"])
             if "people" not in leaflet:
                 continue
             for person_data in leaflet["people"]:
